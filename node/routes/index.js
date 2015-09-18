93e274231d4317f086fa28da099803f1b5a9d7ab
Ben Chang <ben21045@gmail.com>
2015-09-18 15:11:43 -0400

    add node server

---
+++ node/routes/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+router.get('/helloworld', function(req, res) {
+    res.render('helloworld', { title: 'Hello, World!' });
+});
+router.get('/userlist', function(req, res) {
+    var db = req.db;
+    var collection = db.get('usercollection');
+    collection.find({},{},function(e,docs){
+        res.render('userlist', {
+            "userlist" : docs
+        });
+    });
+});
+module.exports = router;
+
