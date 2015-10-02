b91d35fd182169ebfdff83ecb22d0a2c3f41354e
Ben Chang <ben21045@gmail.com>
2015-10-02 15:16:25 -0400

    POST request handling

--- node/routes/index.js
+++ node/routes/index.js
@@ -23,7 +23,41 @@ function renderLogin(req, res) {
 }
 
 router.get('/login', renderLogin);
+router.post('/loginsubmit',function(req, res) {
+    var userName= req.body.username;
+    var password= req.body.password;
+    if((userName.toLowerCase=="ben21045@gmail.com")&&(password=="abc123")){
+        res.redirect("userlist");
+    }
+});
 
+router.post('/adduser', function(req, res) {
+
+    // Set our internal DB variable
+    var db = req.db;
+
+    // Get our form values. These rely on the "name" attributes
+    var userName = req.body.username;
+    var userEmail = req.body.useremail;
+
+    // Set our collection
+    var collection = db.get('usercollection');
+
+    // Submit to the DB
+    collection.insert({
+        "username" : userName,
+        "email" : userEmail
+    }, function (err, doc) {
+        if (err) {
+            // If it failed, return error
+            res.send("There was a problem adding the information to the database.");
+        }
+        else {
+            // And forward to success page
+            res.redirect("userlist");
+        }
+    });
+});
 
 module.exports = router;
 
