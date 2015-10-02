870c979570aa6e93d010a160c5029443b115e354
AlbertZhang <albertrwz@outlook.com>
2015-10-02 15:18:31 -0400

    update login.js

--- node/public/js/login.js
+++ node/public/js/login.js
@@ -3,5 +3,20 @@ $(document).ready(function() {
 	console.log($("#inputEmail"));
 	$("#inputEmail").val("hello@umd.edu");
 	console.log($("#inputEmail").val());
-	
+
+	$('#inputEmail').on("click", function(e) {
+		var v = "";
+		$.ajax({
+			data: {
+				"name": v,
+				...
+			},
+			type: "POST",
+			url: "localhost:3000/loginsubmit",
+			success: functino() {
+				
+			}
+		});
+	});
+
 });
