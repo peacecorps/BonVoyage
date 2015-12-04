08ed0a4d5f06906db5ce4f16992442be166dfd1b
Edwin Tan <etan432@gmail.com>
2015-12-04 17:58:43 -0500

    add selectize and date picker

---
+++ node/public/js/submission.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+     $('.datepicker').pickadate();
+     var $select = $('#select-country').selectize({
+        valueField: 'country_code',
+        labelField: 'country',
+        onInitialize: function() {
+            //Load the JSON file of the countries
+            $.ajax({
+                method: "GET",
+                url: "/data/countryList.json",
+                datatype: "json",
+                success: function(json, status, request) {
+                    console.log(json);
+
+                     var arrCountries = [];
+                     for(var key in json) {
+                        arrCountries.push({ "country": json[key], "country_code": key });
+                    }
+
+                    var selectize = $select[0].selectize;
+                    selectize.addOption(arrCountries);
+                    selectize.addOption("hi");
+                    selectize.refreshOptions();
+
+                    console.log(selectize);
+                    console.log(arrCountries);
+                }
+
+            });
+
+
+
+        }
+     });
+});
