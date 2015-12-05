4c97c0d8c576029c41ce99dbc16dec5d21ba0b01
AlbertZhang <albertrwz@outlook.com>
2015-12-05 12:47:28 -0500

    change ajax call to html form

--- node/config/passport.js
+++ node/config/passport.js
@@ -39,37 +39,36 @@ module.exports = function(passport) {
         // asynchronous
         // User.findOne wont fire unless data is sent back
         process.nextTick(function() {
+            // find a user whose email is the same as the forms email
+            // we are checking to see if the user trying to login already exists
+            User.findOne({ 'email' :  email }, function(err, user) {
+                // if there are any errors, return the error
+                if (err)
+                    return done(err);
 
-        // find a user whose email is the same as the forms email
-        // we are checking to see if the user trying to login already exists
-        User.findOne({ 'email' :  email }, function(err, user) {
-            // if there are any errors, return the error
-            if (err)
-                return done(err);
+                // check to see if theres already a user with that email
+                if (user) {
+                    return done(null, false, req.flash('signupMessage', 'That email is already taken.'));
+                } else {
 
-            // check to see if theres already a user with that email
-            if (user) {
-                return done(null, false, req.flash('signupMessage', 'That email is already taken.'));
-            } else {
+                    // if there is no user with that email
+                    // create the user
+                    var newUser            = new User();
 
-                // if there is no user with that email
-                // create the user
-                var newUser            = new User();
+                    // set the user's local credentials
 
-                // set the user's local credentials
-                
-                newUser.email    = email;
-                newUser.password = password;
+                    newUser.email    = email;
+                    newUser.password = password;
 
-                // save the user
-                newUser.save(function(err) {
-                    if (err)
-                        return done(err);
-                    return done(null, newUser);
-                });
-            }
+                    // save the user
+                    newUser.save(function(err) {
+                        if (err)
+                            return done(err);
+                        return done(null, newUser);
+                    });
+                }
 
-        });    
+            });    
 
         });
 
