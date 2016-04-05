339ac8cd7fd72557d696d74f1b49281619ea631d
Colin <colinking@users.noreply.github.com>
2016-04-04 22:38:53 -0400

    Rollback forcing HTTPs
    * Attempting to fix FastClick issue
    
    * Fixed FastClick production issue
    
    * Force HTTPs
    
    * Rollback forced HTTPs

--- app.js
+++ app.js
@@ -58,16 +58,16 @@ app.use(function (req, res, next) {
 	return next();
 });
 
-// Force https
-if (process.env.NODE_ENV == 'production') {
-	app.use(function (req, res, next) {
-		if (req.headers['x-forwarded-proto'] !== 'https') {
-			return res.redirect(['https://', req.get('Host'), req.url].join(''));
-		}
-
-		return next();
-	});
-}
+// Force https -- need SSL cert first to support custom domain
+// if (process.env.NODE_ENV == 'production') {
+// 	app.use(function (req, res, next) {
+// 		if (req.headers['x-forwarded-proto'] !== 'https') {
+// 			return res.redirect(['https://', req.get('Host'), req.url].join(''));
+// 		}
+//
+// 		return next();
+// 	});
+// }
 
 mongoose.connect(process.env.DATABASE_URL);
 mongoose.connection.on('error', function (err) {
