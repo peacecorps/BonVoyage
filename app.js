a102f37ac46279a6d6190bd1d113da1ec10c9772
Sean Bae <s@seanbae.net>
2016-05-02 12:48:45 -0400

    New error pages are created

--- app.js
+++ app.js
@@ -211,22 +211,26 @@ app.use(function (req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-	app.use(function (err, req, res) {
+	app.use(function (err, req, res, next) {
 		res.status(err.status || 500);
 		res.render('error', {
 			message: err.message,
 			error: err,
+			hideLogout: true,
+			returnButton: process.env.BONVOYAGE_DOMAIN,
 		});
 	});
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function (err, req, res) {
+app.use(function (err, req, res, next) {
 	res.status(err.status || 500);
 	res.render('error', {
 		message: err.message,
 		error: {},
+		hideLogout: true,
+		returnButton: process.env.BONVOYAGE_DOMAIN,
 	});
 });
 
