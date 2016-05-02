76b78fabcada41a4dfd425e1696b0c550f2648bc
Sean Bae <s@seanbae.net>
2016-05-02 12:52:07 -0400

    Ignore jshint warning for error express middleware

--- app.js
+++ app.js
@@ -212,6 +212,7 @@ app.use(function (req, res, next) {
 // will print stacktrace
 if (app.get('env') === 'development') {
 	app.use(function (err, req, res, next) {
+		// jshint unused: false
 		res.status(err.status || 500);
 		res.render('error', {
 			message: err.message,
@@ -225,6 +226,7 @@ if (app.get('env') === 'development') {
 // production error handler
 // no stacktraces leaked to user
 app.use(function (err, req, res, next) {
+	//jshint unused: false
 	res.status(err.status || 500);
 	res.render('error', {
 		message: err.message,
